92072ae753930f526d9dfd04858f67a8851486b8
Matt Major <mattmajor14@gmail.com>
2016-01-31 17:32:01 +0000

    Revert "Add minification, minify output"
    This reverts commit e6c9cf8dfac90a0864b4d50e611039084f383a41.

--- dist/request-helper.js
+++ dist/request-helper.js
@@ -1,1 +1,148 @@
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}}function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(exports,"__esModule",{value:!0});var _createClass=function(){function e(e,t){for(var n=0;n<t.length;n++){var u=t[n];u.enumerable=u.enumerable||!1,u.configurable=!0,"value"in u&&(u.writable=!0),Object.defineProperty(e,u.key,u)}}return function(t,n,u){return n&&e(t.prototype,n),u&&e(t,u),t}}(),_request=require("request"),_request2=_interopRequireDefault(_request),RequestHelper=function(){function e(t){_classCallCheck(this,e),this.headers={authorization:"Bearer "+t,content_type:"application/json"},this.apiUrl="https://api.digitalocean.com/v2/"}return _createClass(e,[{key:"request",value:function(e,t){e.includeAll?this.getAllPages(e.key,e,t):this.submitRequest(e,t)}},{key:"submitRequest",value:function(e,t){var n=this.requestBuilder(e);(0,_request2["default"])(n,function(e,n,u){e?t(e):t(null,n,u)})}},{key:"getAllPages",value:function(e,t,n){var u=[],r=0,a=0,s=1;t.qs.page=1,this.submitRequest(t,function(i,l,o){return i&&n(i),r=o.meta.total,u=u.concat(o[e]),a=r/(t.qs.per_page||25),u.length>=r?n(null,l,u):void this.getRemainingPages(t,2,a,function(t,r,i){t&&n(t),s++,u=u.concat(i[e]),s===a&&n(null,r,u)})}.bind(this))}},{key:"getRemainingPages",value:function(e,t,n,u){for(var r=t;n>=r;r++)e.qs.page=r,this.submitRequest(e,u)}},{key:"requestBuilder",value:function(e){return{uri:this.apiUrl+e.actionPath,method:e.method||"GET",headers:e.headers||this.headers,body:e.body||{},strictSSL:!0,json:!0,qs:e.qs||{}}}}]),e}();exports["default"]=RequestHelper,module.exports=exports["default"];
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var RequestHelper = (function () {
+  /**
+   * Request Helper
+   * @param {string} token - Your Private API Token
+   * @constructor
+   */
+
+  function RequestHelper(token) {
+    _classCallCheck(this, RequestHelper);
+
+    this.headers = {
+      'authorization': 'Bearer ' + token,
+      'content_type': 'application/json'
+    };
+    this.apiUrl = 'https://api.digitalocean.com/v2/';
+  }
+
+  /**
+   * Check the required Request & Trigger
+   * @param {*} options - Request Options
+   * @param {*} callback - Function to execute on completion
+   */
+
+  _createClass(RequestHelper, [{
+    key: 'request',
+    value: function request(options, callback) {
+      if (options.includeAll) {
+        this.getAllPages(options.key, options, callback);
+      } else {
+        this.submitRequest(options, callback);
+      }
+    }
+
+    /**
+     * Submit the Request
+     * @param {*} options - Request Options Object
+     * @param {*} callback - Function to execute on completion
+     */
+  }, {
+    key: 'submitRequest',
+    value: function submitRequest(options, callback) {
+      var requestOptions = this.requestBuilder(options);
+      (0, _request2['default'])(requestOptions, function (err, response, body) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, response, body);
+        }
+      });
+    }
+
+    /**
+     * Get All Pages
+     * @param {string} key - Type of Item
+     * @param {*} options - Request Options
+     * @param {*} callback - Function to execute on completion
+     */
+  }, {
+    key: 'getAllPages',
+    value: function getAllPages(key, options, callback) {
+      var items = [],
+          total = 0,
+          required = 0,
+          completed = 1;
+
+      options.qs.page = 1;
+
+      this.submitRequest(options, (function (err, response, body) {
+        if (err) {
+          callback(err);
+        }
+        total = body.meta.total;
+        items = items.concat(body[key]);
+        required = total / (options.qs.per_page || 25);
+        if (items.length >= total) {
+          return callback(null, response, items);
+        } else {
+          this.getRemainingPages(options, 2, required, function (err, response, body) {
+            if (err) {
+              callback(err);
+            }
+            completed++;
+            items = items.concat(body[key]);
+            if (completed === required) {
+              callback(null, response, items);
+            }
+          });
+        }
+      }).bind(this));
+    }
+
+    /**
+     * Get the Remaining Pages
+     * @param {*} options - Request Options
+     * @param {number} first - The first page to retrieve
+     * @param {number} last - The last page to retrieve
+     * @param {*} callback - Function to execute on completion
+     */
+  }, {
+    key: 'getRemainingPages',
+    value: function getRemainingPages(options, first, last, callback) {
+      for (var current = first; current <= last; current++) {
+        options.qs.page = current;
+        this.submitRequest(options, callback);
+      }
+    }
+
+    /**
+     * Build Options for Request
+     * @param {*} options - Options Object
+     * @returns {*}
+     */
+  }, {
+    key: 'requestBuilder',
+    value: function requestBuilder(options) {
+      return {
+        uri: this.apiUrl + options.actionPath,
+        method: options.method || 'GET',
+        headers: options.headers || this.headers,
+        body: options.body || {},
+        strictSSL: true,
+        json: true,
+        qs: options.qs || {}
+      };
+    }
+  }]);
+
+  return RequestHelper;
+})();
+
+exports['default'] = RequestHelper;
+module.exports = exports['default'];
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
-    uglify = require('gulp-uglify'),
     SOURCE_DIRECTORY = 'src/**/*.js',
     DEST_DIRECTORY = 'dist/';
 
@@ -9,6 +8,5 @@ gulp.task('default', ['compile']);
 gulp.task('compile', function () {
   return gulp.src(SOURCE_DIRECTORY)
           .pipe(babel({experimental: true}))
-          .pipe(uglify())
           .pipe(gulp.dest(DEST_DIRECTORY));
 });
