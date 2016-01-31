9bef1cf127740bd264bd8753df0e09e23ba89ce9
Matt Major <mattmajor14@gmail.com>
2016-01-31 17:37:07 +0000

    Re-adding minfication

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    SOURCE_DIRECTORY = 'src/**/*.js',
-    DEST_DIRECTORY = 'dist/';
+var gulp              = require('gulp'),
+    babel             = require('gulp-babel'),
+    uglify            = require('gulp-uglify'),
+    SOURCE_DIRECTORY  = 'src/**/*.js',
+    DEST_DIRECTORY    = 'dist/';
 
 gulp.task('default', ['compile']);
 
 gulp.task('compile', function () {
   return gulp.src(SOURCE_DIRECTORY)
           .pipe(babel({experimental: true}))
+          .pipe(uglify())
           .pipe(gulp.dest(DEST_DIRECTORY));
 });
