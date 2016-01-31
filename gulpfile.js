e6c9cf8dfac90a0864b4d50e611039084f383a41
Matt Major <mattmajor14@gmail.com>
2016-01-31 17:25:23 +0000

    Add minification, minify output

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
     SOURCE_DIRECTORY = 'src/**/*.js',
     DEST_DIRECTORY = 'dist/';
 
@@ -8,5 +9,6 @@ gulp.task('default', ['compile']);
 gulp.task('compile', function () {
   return gulp.src(SOURCE_DIRECTORY)
           .pipe(babel({experimental: true}))
+          .pipe(uglify())
           .pipe(gulp.dest(DEST_DIRECTORY));
 });
